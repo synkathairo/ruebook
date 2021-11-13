46f2d77e9e545dbb5e64716f7563605ec731497b
synkathairo <>
2021-11-13 15:33:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
-	var host = server.address().address
-	var port = server.address().port
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
 
-	console.log("Example app listening at http://%s:%s", host, port)
+// Creates a client
+const client = new language.LanguageServiceClient();
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
 })
