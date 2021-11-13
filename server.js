37e1d112d4eca048b86e47fdb7702bd6a00c5770
synkathairo <>
2021-11-13 15:12:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.get('/', function (req, res) {
 })
 
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log("Example app listening at http://%s:%s", host, port)
 })
