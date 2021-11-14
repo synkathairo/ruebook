e4814f23fb76bfe89aa8b22ced60dcb948b1b363
synkathairo <>
2021-11-14 02:09:57 -0500

    update server to have post ability from journal entry

--- server.js
+++ server.js
@@ -1,21 +1,58 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+// const { response } = require('express');
+// var { response } = require('express');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
-
+app.get('/index.html', function (req, res) {
+	res.sendFile( __dirname + "/" + "index.html" );
+	// res.send('hello world');
+})
 app.get('/', function (req, res) {
-   res.send('Hello World');
+	res.sendFile( __dirname + "/" + "index.html" );
+	// res.send('hello world');
+})
+app.get('/journal.html', function (req, res) {
+	res.sendFile( __dirname + "/" + "journal.html" );
+	// res.send('hello world');
+})
+app.get('/about.html', function (req, res) {
+	res.sendFile( __dirname + "/" + "about.html" );
+	// res.send('hello world');
 })
 
-// Imports the Google Cloud client library
-const language = require('@google-cloud/language');
+app.post('/process_post', urlencodedParser, function (req, res) {
+	// Prepare output in JSON format
+	response = {
+		// first_name:req.query.first_name,
+		// last_name:req.query.last_name
+		journalEntry:req.body.journalEntry
+	};
+	// response = req.query.journalEntry;
+	console.log(response);
+	res.end(JSON.stringify(response));
+	// app.get('/journal.html', function (req, res) {
+	// 	res.sendFile( __dirname + "/" + "journal.html" );
+	// 	res.send(response);
+	// })
+})
 
-// Creates a client
-const client = new language.LanguageServiceClient();
+// app.post('/process_post', urlencodedParser, function (req, res) {
+//    // Prepare output in JSON format
+//    response = {
+//       first_name:req.body.first_name,
+//       last_name:req.body.last_name
+//    };
+//    console.log(response);
+//    res.end(JSON.stringify(response));
+// })
 
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("Example app listening at http://%s:%s", host, port)
+	var host = server.address().address
+	var port = server.address().port
+	
+	console.log("Example app listening at http://%s:%s", host, port)
 })
